da76323e5e2203600e990850807583cfacc372ab
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-03-13 23:36:40 -0300

    Exercicio Personagens

---
+++ JS-Aula/JS V - POO/01 - exercicios/characters/index.js
@@ -0,0 +1,23 @@
+const Mage = require("./Mage");
+const Thief = require("./Thief");
+const Warrior = require("./Warrior");
+
+const arthur = new Mage('Arthur',90,4,2,14)
+const beatrice = new Thief('Beatrice',140,22,8)
+const cain = new Warrior('Cain',120,14,12,4)
+
+console.table({arthur,beatrice,cain})
+
+cain.switchStance()
+arthur.attack(cain)
+beatrice.attack(arthur)
+
+
+console.table({arthur,beatrice,cain})
+
+cain.switchStance()
+cain.attack(arthur)
+arthur.heal(arthur)
+
+console.table({arthur,beatrice,cain})
+
