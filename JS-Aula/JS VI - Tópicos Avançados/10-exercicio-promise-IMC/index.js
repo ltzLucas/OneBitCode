4178ab01ddccf981bd3d36b37ca3ac8750e1dcc8
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-03-21 16:51:48 -0300

    Arrumando alguns erros de códigos

--- JS-Aula/JS VI - Tópicos Avançados/10-exercicio-promise-IMC/index.js
+++ JS-Aula/JS VI - Tópicos Avançados/10-exercicio-promise-IMC/index.js
@@ -9,13 +9,13 @@ function IMC(weight,height){
                 case imc < 18.5:
                     status = 'magreza'
                     break
-                case img >= 18.5 && imc < 25:
+                case imc >= 18.5 && imc < 25:
                     status = 'normal'
                     break
                 case imc >= 25 && imc < 30:
                     status = 'sobrepeso'
                     break
-                case img >= 30 && imc < 40:
+                case imc >= 30 && imc < 40:
                     status = 'obesidade'
                     break
                 default:
@@ -32,15 +32,13 @@ function execute(weight,height){
         console.log(result)
     }).catch((err) =>{
         console.log(`A função não foi executada corretamente motivo: ${err}`)
-    }).finally(() => {
-        console.log('Função foi finalizada')
     })
-    console.log('ESTA DEPOIS QUE EU CHAMEI TUDO')
+    console.log(`Calculando o IMC para peso ${weight} e altura ${height}... `)
 
 }
 
-execute(1.85,90)
-execute(1.60,90)
-execute(1.85,95)
-execute(1.85,"texto")
-execute(1.85,"15")
+execute(71,1.74)
+execute(48,1.60)
+execute("texto",71)
+execute(82,1.72)
+execute(120,1.80)
