c69bbc6583c63b372b400af7518694be563174d1
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-03-25 22:33:02 -0300

    await

---
+++ JS-Aula/JS VI - Tópicos Avançados/17-utilizando-await/index.js
@@ -0,0 +1,26 @@
+async function asyncSum(a,b){
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        return Promise.reject('arguments for sum must be of type number')
+    }
+    return a + b
+}
+
+
+// async function execute(){
+//     asyncSum(50,33).then(result => {
+//         console.log(result)
+//     })
+// }
+
+async function execute(){
+
+    try {
+        const result = await asyncSum(50,null)
+        console.log(result)
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+
+execute()
