5951e4d7f597954c276b681586a93c910da923c0
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-05-25 13:52:12 -0300

    Projeto em React

---
+++ React/first-react-project/src/App.jsx
@@ -0,0 +1,18 @@
+export default function App(){
+  return (
+
+    <div>
+      <h1>Olá, Mundo!</h1>
+      <p>Este é meu primeiro projeto usando React e Vit</p>
+      <p>Proximos passos:</p>
+      <u>
+        <li>Praticar com exercicios</li>
+        <li>Aprender a usar dados dinamicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar componentes</li>
+
+      </u>
+    </div>
+  )
+}
