1aeca4b076de27469a59f88b6bfc6d2822cad1d0
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-05-25 16:39:45 -0300

    Fragment

--- React/exercicios/Exercicio02/src/App.jsx
+++ React/exercicios/Exercicio02/src/App.jsx
@@ -2,14 +2,16 @@ import Card from "./components/Card";
 
 export default function App(){
   return (
-    <div>
+    <>
+
       <Card >
       </Card>
       <Card />
       <Card />
       <Card />
       <Card />
-    </div>
+    </>
+
 
   )
 
