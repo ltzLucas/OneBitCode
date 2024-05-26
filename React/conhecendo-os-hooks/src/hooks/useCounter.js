56956f50125894158d938dd3f3c8d022b6d0e56a
ltzLucas <ltzlucasteixeiralima@gmail.com>
2024-05-26 10:13:20 -0300

    Hooks

--- React/conhecendo-os-hooks/src/hooks/useCounter.js
+++ React/conhecendo-os-hooks/src/hooks/useCounter.js
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 
 
+function getInititalValue(){
+    console.log("obtendo o valor inicial")
+    return 1 + 1
+}
+
 export default function userCounter(){
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(() => getInititalValue())
 
     const increment = () =>{
         setCount(count + 1)
